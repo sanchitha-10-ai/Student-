e7931a53f75aa370422cbd54758c3e80473bb48d
Sanchitha Aashish <Saashish@v2soft.com>
2026-03-11 17:52:46 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Program started");
+
+        Task task1 = DownloadData();
+        Task task2 = ProcessData();
+
+        Console.WriteLine("Main method continues working...");
+
+        await task1;
+        await task2;
+
+        Console.WriteLine("Program finished");
+    }
+
+    static async Task DownloadData()
+    {
+        Console.WriteLine("Downloading data...");
+
+        await Task.Delay(3000); // simulate 3 second work
+
+        Console.WriteLine("Download complete");
+    }
+
+    static async Task ProcessData()
+    {
+        Console.WriteLine("Processing data...");
+
+        await Task.Delay(2000); // simulate 2 second work
+
+        Console.WriteLine("Processing finished");
+    }
+}
